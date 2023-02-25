d0016b77d83cd0d53f420b2b1019bb38e971badf
Sebastian T F <sebastian.tharakan97@gmail.com>
2023-02-25 23:03:13 +0530

    refactor: ♻️ print to stderror

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,14 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(args).unwrap_or_else(|error| {
-        println!("Error preparing config: {}", error);
+        eprintln!("Error preparing config: {}", error);
         process::exit(1)
     });
 
     println!("{:#?}", config);
 
     minigrep::run(config).unwrap_or_else(|error| {
-        println!("Error running: {}", error);
+        eprintln!("Error running: {}", error);
         process::exit(1)
     });
 }
